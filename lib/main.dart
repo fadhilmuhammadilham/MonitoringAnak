e369bfe256d194830a37030a2ceeeb164655df2f
Daffa Fahrizal <sanamntzk96@gmail.com>
2024-03-30 05:35:10 +0700

    refactor: mengubah default home ke bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:the_project/page/home_page.dart';
+import 'package:the_project/component/bottom_nav_bar.dart';
+// import 'package:the_project/page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Presence Absence',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const BottomNavBar(),
     );
   }
 }
