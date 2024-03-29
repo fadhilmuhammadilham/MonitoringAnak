153d9ceee0d780f5eee14c84cbf2ef2ff03938c1
Daffa Fahrizal <sanamntzk96@gmail.com>
2024-03-30 05:35:51 +0700

    feat: menambahkan halaman rekap

---
+++ lib/page/rekap_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Rekap extends StatefulWidget {
+  const Rekap({super.key});
+
+  @override
+  State<Rekap> createState() => _RekapState();
+}
+
+class _RekapState extends State<Rekap> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Ini Halaman Rekap')
+      ),
+    );
+  }
+}
